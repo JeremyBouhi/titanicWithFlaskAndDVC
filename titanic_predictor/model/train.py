3792bce42795505edb8990f07cebcdb69a37d783
Jeremy Bouhi <jeremybh2406@gmail.com>
2019-05-22 14:25:31 +0200

    V3-BayesianRidge

--- titanic_predictor/model/train.py
+++ titanic_predictor/model/train.py
@@ -1,5 +1,5 @@
 import pickle
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import BayesianRidge
 
 try: from titanic_predictor import conf
 except: import conf
@@ -12,7 +12,7 @@ with open(conf.X_train, 'rb') as fd:
 with open(conf.y_train, 'rb') as fd:
     y_train = pickle.load(fd)
 
-reg = RandomForestRegressor()
+reg = BayesianRidge()
 reg.fit(X_train, y_train)
 print(reg)
 
